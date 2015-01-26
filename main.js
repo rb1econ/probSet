fe894c9c264d55efff509b3026b0326125299a7d
Ryan budny <rb1econ@gmail.com>
2015-01-26 15:37:58 -0700

    up to bonus

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var letterCapitalize = function(string){
+// var letterCapitalize = function(string){
 	
-	var splitString = string.split('');
+// 	var splitString = string.split('');
 	
-	var upCased = splitString[0].toUpperCase()
+// 	var upCased = splitString[0].toUpperCase()
 
-	splitString.splice(0, 1, upCased);
+// 	splitString.splice(0, 1, upCased);
 
 
-	// console.log(splitString);
+// 	// console.log(splitString);
 
 	
 
@@ -18,18 +18,30 @@ var letterCapitalize = function(string){
 
 		
 
-		for (var i=0; i<splitString.length-1; i++) {
-				if (splitString[i] === ' ') {
-					var splitCap = splitString[i+1].toUpperCase();
-					var capReplaced = splitString.splice((i+1),1,splitCap);
-					// console.log(splitString);
+// 		for (var i=0; i<splitString.length-1; i++) {
+// 				if (splitString[i] === ' ') {
+// 					var splitCap = splitString[i+1].toUpperCase();
+// 					var capReplaced = splitString.splice((i+1),1,splitCap);
+// 					// console.log(splitString);
 
-			}
-		}
+// 			}
+// 		}
 	
-	return splitString.join('');
+// 	return splitString.join('');
 	
+// }
+// console.log(letterCapitalize('hella world yo'));
+
+
+// PROBLEM TWO
+
+
+
+var wordCount = function(str){
+	var wordArr = str.split(' ');
+	return wordArr.length;
 }
 
+console.log(wordCount("this is a string of a number of words"));
 
-console.log(letterCapitalize('hella world yo'));
+
